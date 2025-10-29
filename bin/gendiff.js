b08739fc502485ef4200a775e27ce9aa1474fc7e
Neyilanau <halapovalbek@gmail.com>
2025-10-29 16:38:49 +0300

    Two steps

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-
-import { program } from 'commander';
-
+import { Command } from 'commander';
+const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'display help for command')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing ${filepath1} and ${filepath2} with format ${options.format || 'stylish'}`);
+  });
+program.parse();
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
 export default function genDiff(filepath1, filepath2) {
+  // Заглушка: пока просто возвращаем строку
   return `Difference between ${filepath1} and ${filepath2}`;
 }
